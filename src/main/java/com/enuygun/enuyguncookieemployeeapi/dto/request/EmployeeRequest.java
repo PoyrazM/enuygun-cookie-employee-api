2633736e86b6e530553a2d54f28821af8f43ea3a
Mertcan Poyraz <mertcan_1999@icloud.com>
2023-08-02 09:43:37 +0300

    Validation Messages moved to properties file

--- src/main/java/com/enuygun/enuyguncookieemployeeapi/dto/request/EmployeeRequest.java
+++ src/main/java/com/enuygun/enuyguncookieemployeeapi/dto/request/EmployeeRequest.java
@@ -3,32 +3,34 @@ package com.enuygun.enuyguncookieemployeeapi.dto.request;
 import jakarta.validation.constraints.*;
 
 public record EmployeeRequest(
-        @NotNull(message = "First Name should not be null !")
-        @Size(min = 2, message = "First Name should not be less than 2 characters !")
+        @NotNull(message = "{enuygun-first-name-null-message}")
+        @Size(min = 2, message = "{enuygun-first-name-size-message}")
         String firstName,
 
-        @NotNull(message = "Last Name should not be null !")
-        @Size(min = 2, message = "Last Name should not be less than 2 characters !")
+        @NotNull(message = "{enuygun-last-name-null-message}")
+        @Size(min = 2, message = "{enuygun-last-name-size-message}")
         String lastName,
 
-        @NotNull(message = "Username should not be null !")
-        @Size(min = 4, message = "Username should not be less than 4 characters !")
+        @NotNull(message = "{enuygun-username-null-message}")
+        @Size(min = 4, message = "{enuygun-username-size-message}")
         String username,
 
-        @NotNull(message = "Email should not be null !")
-        @Pattern(regexp = "^(.+)@(\\S+)$", message = "Invalid Email Address !")
+        @NotNull(message = "{enuygun-email-size-message}")
+        @Pattern(regexp = "^(.+)@(\\S+)$", message = "{enuygun-invalid-email-message}")
         String email,
 
-        @NotNull(message = "Password should not be null !")
+        @NotNull(message = "{enuygun-password-null-message}")
+        @Size(min = 6, message = "{enuygun-password-size-message}")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{enuygun-password-pattern-message}")
         String password,
 
-        @NotNull(message = "Phone Number should not be null !")
-        @Size(min = 10, message = "Phone Number should not be less than 10 characters !")
+        @NotNull(message = "{enuygun-phone-number-null-message}")
+        @Size(min = 10, message = "{enuygun-phone-number-size-message}")
         String phone,
 
-        @NotNull(message = "Job should not be null !")
+        @NotNull(message = "{enuygun-job-null-message}")
         String job,
 
-        @NotNull(message = "Department should not be null !")
+        @NotNull(message = "{enuygun-department-null-message}")
         String department
 ){}
